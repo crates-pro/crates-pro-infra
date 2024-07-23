6675b022afb1d65120f46e116c0bb7eef0a49999
Yuxuan Dai <yxdai@smail.nju.edu.cn>
2024-07-23 15:50:06 +0800

    fix: Use package renaming for multi-version dependencies
    * Remove redundant third-party-extra folder
    * Create package alias in Cargo.toml

--- gendeps.sh
+++ gendeps.sh
@@ -2,19 +2,8 @@
 set -e
 # Generate BUCK files for third-party dependencies
 reindeer --third-party-dir third-party buckify
-reindeer --third-party-dir third-party-extra buckify
 
 # Additional fixup for //third-party:mime_guess-2.0.x
 old_env='"OUT_DIR": "\$(location :mime_guess-\(2\.0\.[0-9]\+\)-build-script-run\[out_dir\])",'
 new_env='"MIME_TYPES_GENERATED_PATH": "\$(location :mime_guess-\1-build-script-run\[out_dir\])/mime_types_generated\.rs",'
 sed -i "s|$old_env|$new_env|" third-party/BUCK
-
-# Depend on "//third-party:clap-v4" for clap-4.x.x
-# Depend on "//third-party:clap" for clap-3.x.x
-cat << EOF >> third-party/BUCK
-alias(
-    name = "clap-v4",
-    actual = "//third-party-extra:clap",
-    visibility = ["PUBLIC"],
-)
-EOF
