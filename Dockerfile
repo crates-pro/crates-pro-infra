43bea47b6b86dae7969ed5994954b3de7c19f5fb
Yuxuan Dai <yxdai@smail.nju.edu.cn>
2024-09-03 15:27:14 +0800

    feat(deploy): Add Dockerfile for Crates Pro deployment
    * Create a new Dockerfile for deploying Crates Pro
    * Move performance-benchmark runtimes dependencies to the new Dockerfile
    * Update submodules
    
    Signed-off-by: Yuxuan Dai <yxdai@smail.nju.edu.cn>

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,17 @@
 FROM --platform=linux/amd64 almalinux:8.10-20240723
 
-# Install necessary build tools
-RUN dnf group install -y "Development Tools" \
-    && dnf install -y curl
+# Install tools and dependencies
+# Reindeer: openssl-devel
+RUN dnf update -y \
+    && dnf group install -y "Development Tools" \
+    && dnf install -y curl openssl-devel
 
 # Install Rust with nightly-2024-06-08 toolchain (required by Buck2)
 RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly-2024-06-08 -y
 ENV PATH="/root/.cargo/bin:$PATH"
 
 # Install Buck2 and Reindeer
-RUN cargo install --git https://github.com/facebook/buck2.git --rev 2d341993 buck2
-RUN dnf install -y openssl-devel
-RUN cargo install --git https://github.com/facebookincubator/reindeer.git --rev 7cfdcaf7 reindeer
-
-# Install performance-benchmark test/runtime dependencies
-RUN curl -O https://www.python.org/ftp/python/3.11.9/Python-3.11.9.tgz \
-    && tar -xzf Python-3.11.9.tgz \
-    && cd Python-3.11.9 \
-    && ./configure --enable-optimizations --prefix=/opt \
-    && make -j8 \
-    && make install \
-    && cd .. \
-    && /opt/bin/python3 -m pip install --upgrade pip \
-    && /opt/bin/python3 -m pip install kaleido matplotlib plotly \
-    && rm -rf Python-3.11.9.tgz Python-3.11.9
-RUN ln -s /opt/bin/python3 /usr/local/bin/python
+RUN cargo install --git https://github.com/facebook/buck2.git --rev 7764d721 buck2
+RUN cargo install --git https://github.com/facebookincubator/reindeer.git --rev 2bbaed25 reindeer
 
 WORKDIR /workdir
--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 set -e
 
+# Build a target and copy the artifact to a destination
 build_and_copy() {
     local target="$1"
     local dest="$2"
@@ -8,23 +9,12 @@ build_and_copy() {
     cp "$output" "$dest"
 }
 
-# Setup
-rm -rf build
-mkdir -p build/crates-pro build/performance-benchmark build/sensleak-rs
+# Cleanup and prepare the destination directories
+rm -rf build/crates-pro
+mkdir -p build/crates-pro
 
-# crates-pro
+# crates-pro executables
 build_and_copy "//project/crates-pro:crates_pro" "build/crates-pro/crates_pro"
-build_and_copy "//project/crates-pro/tuplugins:plugin1" "build/crates-pro/plugin1.so"
-build_and_copy "//project/crates-pro/tuplugins:plugin2" "build/crates-pro/plugin2.so"
 
-# performance-benchmark
-build_and_copy "//project/performance-benchmark:collector" "build/performance-benchmark/collector"
-build_and_copy "//project/performance-benchmark:flamegraph-fake" "build/performance-benchmark/flamegraph-fake"
-build_and_copy "//project/performance-benchmark:muti-rustc-perf" "build/performance-benchmark/muti-rustc-perf"
-build_and_copy "//project/performance-benchmark:runtime-fake" "build/performance-benchmark/runtime-fake"
-build_and_copy "//project/performance-benchmark:rustc-fake" "build/performance-benchmark/rustc-fake"
-build_and_copy "//project/performance-benchmark:manager" "build/performance-benchmark/manager"
-
-# sensleak-rs
-build_and_copy "//project/sensleak-rs:api" "build/sensleak-rs/api"
-build_and_copy "//project/sensleak-rs:scan" "build/sensleak-rs/scan"
+# crates-pro runtime dependencies
+cp project/crates-pro/.env build/crates-pro
